da03fb7fe65c89b52d0cddd2198f9bd714c75aca
franckkoukougnon <franckkoukougnon@gmail.com>
2020-06-04 11:38:00 +0200

    Methode Dom Js

---
+++ simple.js
@@ -0,0 +1,8 @@
+let header = document.getElementById('mainHeader');
+let active = document.getElementsByClassName ('active');
+let links = document.getElementsByTagName('a');
+let footer = document.getElementById('mainFooter');
+let lastPar = footer.getElementsByTagName('p')
+
+
+console.log(footer);
